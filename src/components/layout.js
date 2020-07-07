07b47dc4e30dc1dbccfa27f7633bc504062d8b8f
Dare Oni <dareoni@Dares-MacBook-Pro.local>
2020-07-07 21:54:44 +0100

    Made some component clean up on the header

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Header from "./header-component/header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -29,8 +29,9 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          // maxWidth: 1224,
+          padding: `64px 108px`,
+          backgroundColor: 'red',
         }}
       >
         <main>{children}</main>
